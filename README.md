63554118a715accd34d58d80a9780582c8b8141f
Paul Chafetz <pchafetz@umich.edu>
2022-11-07 23:39:25 -0500

    Move markers to an array for easier manipulation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Huron-River-Subway-Prototype
 Simple HTML/CSS Prototype of the Huron River Watershed Subway Map
+
+The subway map is fully rendered using Mapbox GL JS. You can learn more about the Mapbox APIs [here](https://docs.mapbox.com/mapbox-gl-js/guides/).
+
+<br />
+
+Authored by: Ryan Baxter and Paul Chafetz
--- js/index.js
+++ js/index.js
@@ -1,113 +1,84 @@
 // accessToken taken from https://codepen.io/bradleyboy/pen/Yryybq
 mapboxgl.accessToken = "pk.eyJ1IjoiYnJhZGRhaWx5IiwiYSI6ImNqN21iam90ZzJ3MnEzM3F1anNkNWIydjMifQ.Dez6MhslaJs8ROSplWPSpQ";
 
-const bounds = [
-    [-90.168537, 41.765717], // Southwest coordinates
-    [-80.164952, 45.709707] // Northeast coordinates
-];
-
 const mapboxSettings = {
     container: "map", // targets this div ID
     style: "mapbox://styles/mapbox/streets-v11", // view with streets
-    center: [-83.7, 42.6], // [lg, lt] north of Lansing
-    zoom: 8, // approx the size of Michigan
-    maxBounds: bounds, // should set a maxbounds 
-    // projection: "globe", // display as a globe
-    interactive: true, // disable user interaction
-    // optionally, use bounds/maxBounds instead of disabling interactivity
+    center: [-83.7, 42.6], // [lg, lt] east of Lansing
+    zoom: 8, // zoom covers central east Michigan
+    maxBounds: [
+        [-90.168537, 41.765717], // southwest coordinates
+        [-80.164952, 45.709707], // northeast coordinates
+    ],
 };
 
 const map = new mapboxgl.Map(mapboxSettings);
 
-const bigLake = new mapboxgl.Marker()
+const markers = [
+    // Big Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.51909881189316, 42.721293972977314])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Big Lake Dam</p>
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Big Lake Dam</p>`)),
 
-const pontiacLake = new mapboxgl.Marker()
+    // Pontiac Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.45103241607949, 42.664682472492984])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Pantiac Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Pantiac Lake Dam</p>`)),
 
-const oxbowLake = new mapboxgl.Marker()
+    // Oxbow Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.47983124810104, 42.645980588668685])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Oxbow Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Oxbow Lake Dam</p>`)),
 
-const cedarIslandLake = new mapboxgl.Marker()
+    // Cedar Island Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.48099571145308, 42.63039758294164])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Cedar Island Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Cedar Island Lake Dam</p>`)),
 
-const southCommerceLake = new mapboxgl.Marker()
+    // South Commerce Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.49968514670081, 42.57435407009425])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Commerce Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Commerce Dam</p>`)),
 
-const northCommerceLake = new mapboxgl.Marker()
+    // North Commerce Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.48986218111924, 42.58293304302951])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Commerce Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Commerce Dam</p>`)),
 
-const proudLake = new mapboxgl.Marker()
+    // Proud Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.52097367057802, 42.56785846280867])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Proud Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Proud Lake Dam</p>`)),
 
-const hubbellPound = new mapboxgl.Marker()
+    // Hubbell Pound
+    new mapboxgl.Marker()
     .setLngLat([-83.61555794821616, 42.58579168920394])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Hubbell Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Hubbell Dam</p>`)),
 
-const kentLake = new mapboxgl.Marker()
+    // Kent Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.64676344950117, 42.5289401382445])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Kent Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Kent Lake Dam</p>`)),
 
-const baseLineLake = new mapboxgl.Marker()
+    // Base Line Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.8926796865478, 42.425370235294935])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Flook Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Flook Dam</p>`)),
 
-const bartonPond = new mapboxgl.Marker()
+    // Barton Pond
+    new mapboxgl.Marker()
     .setLngLat([-83.7584324082047, 42.31377611849595])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Barton Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Barton Dam</p>`)),
 
-const fordLake = new mapboxgl.Marker()
+    // Ford Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.60534047890552, 42.21892656979439])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>Ford Lake Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>Ford Lake Dam</p>`)),
 
-const bellevilleLake = new mapboxgl.Marker()
+    // Belleville Lake
+    new mapboxgl.Marker()
     .setLngLat([-83.49774610909319, 42.213428101123355])
-    .setPopup(new mapboxgl.Popup().setHTML(`
-        <p><b>Dam: </b>French Landing Dam</p> 
-        `))
-    .addTo(map);
+    .setPopup(new mapboxgl.Popup().setHTML(`<p><b>Dam: </b>French Landing Dam</p>`)),
+];
 
-
+markers.forEach(marker => marker.addTo(map));
