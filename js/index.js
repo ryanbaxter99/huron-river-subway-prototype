676007026e260e332231825c1e70fb9fc37ea16b
Paul Chafetz <pchafetz@umich.edu>
2022-11-13 18:50:03 -0500

    Add custom marker and CSS

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const mapboxSettings = {
         [-90.168537, 41.765717], // southwest coordinates
         [-80.164952, 45.709707], // northeast coordinates
     ],
+    maxZoom: 12,
 };
 
 const map = new mapboxgl.Map(mapboxSettings);
@@ -17,18 +18,18 @@ const map = new mapboxgl.Map(mapboxSettings);
 const markers = [
     // Big Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.51909881189316, 42.721293972977314])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.51909881189316, 42.721293972977314])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Big Lake</b></h2> 
         <p><b>Dam: </b>Big Lake Dam</p> 
         <p><b>Lake Size: </b>215 acres</p> 
         <p><b>Max Depth </b>13ft</p>`
-    )),
+        )),
 
     // Pontiac Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.45103241607949, 42.664682472492984])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.45103241607949, 42.664682472492984])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Pontiac Lake</b></h2> 
         <p><b>Dam: </b>Pantiac Lake Dam</p> 
         <p><b>Lake Size: </b>590 acres</p 
@@ -37,8 +38,8 @@ const markers = [
 
     // Oxbow Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.47983124810104, 42.645980588668685])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.47983124810104, 42.645980588668685])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Oxbow Lake Lake</b></h2> 
         <p><b>Dam: </b>Oxbow Lake Dam</p> 
         <p><b>Lake Size: </b>280 acres</p> 
@@ -47,8 +48,8 @@ const markers = [
 
     // Cedar Island Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.48099571145308, 42.63039758294164])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.48099571145308, 42.63039758294164])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Cedar Island Lake</b></h2> 
         <p><b>Dam: </b>Cedar Island Lake Dam</p> 
         <p><b>Lake Size: </b>114 acres</p> 
@@ -57,8 +58,8 @@ const markers = [
 
     // South Commerce Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.49968514670081, 42.57435407009425])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.49968514670081, 42.57435407009425])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>South Commerce Lake</b></h2> 
         <p><b>Dam: </b>Commerce Dam</p> 
         <p><b>Lake Size: </b>236 acres</p> 
@@ -67,8 +68,8 @@ const markers = [
 
     // North Commerce Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.48986218111924, 42.58293304302951])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.48986218111924, 42.58293304302951])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>North Commerce Lake</b></h2> 
         <p><b>Dam: </b>Commerce Dam</p> 
         <p><b>Lake Size: </b>236 acres</p> 
@@ -77,8 +78,8 @@ const markers = [
 
     // Proud Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.52097367057802, 42.56785846280867])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.52097367057802, 42.56785846280867])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Proud Lake</b></h2> 
         <p><b>Dam: </b>Proud Lake Dam</p> 
         <p><b>Lake Size: </b>104 acres</p>
@@ -87,8 +88,8 @@ const markers = [
 
     // Hubbell Pound
     new mapboxgl.Marker()
-    .setLngLat([-83.61555794821616, 42.58579168920394])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.61555794821616, 42.58579168920394])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Hubbell Lake</b></h2> 
         <p><b>Dam: </b>Hubbell Dam</p> 
         <p><b>Lake Size: </b> acres</p>
@@ -97,8 +98,8 @@ const markers = [
 
     // Kent Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.64676344950117, 42.5289401382445])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.64676344950117, 42.5289401382445])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Kent Lake</b></h2> 
         <p><b>Dam: </b>Kent Lake Dam</p> 
         <p><b>Lake Size: </b>1005 acres</p>
@@ -107,8 +108,8 @@ const markers = [
 
     // Base Line Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.8926796865478, 42.425370235294935])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.8926796865478, 42.425370235294935])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Base Line Lake</b></h2> 
         <p><b>Dam: </b>Flook Dam</p> 
         <p><b>Lake Size: </b>255 acres</p>
@@ -117,8 +118,8 @@ const markers = [
 
     // Barton Pond
     new mapboxgl.Marker()
-    .setLngLat([-83.7584324082047, 42.31377611849595])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.7584324082047, 42.31377611849595])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Barton Lake</b></h2> 
         <p><b>Dam: </b>Barton Dam</p> 
         <p><b>Lake Size: </b>302 acres</p>
@@ -127,8 +128,8 @@ const markers = [
 
     // Ford Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.60534047890552, 42.21892656979439])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.60534047890552, 42.21892656979439])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Ford Lake</b></h2> 
         <p><b>Dam: </b>Ford Lake Dam</p> 
         <p><b>Lake Size: </b>975 acres</p>
@@ -137,8 +138,8 @@ const markers = [
 
     // Belleville Lake
     new mapboxgl.Marker()
-    .setLngLat([-83.49774610909319, 42.213428101123355])
-    .setPopup(new mapboxgl.Popup().setHTML(`
+        .setLngLat([-83.49774610909319, 42.213428101123355])
+        .setPopup(new mapboxgl.Popup().setHTML(`
         <h2><b>Belleville Lake</b></h2> 
         <p><b>Dam: </b>French Landing Dam</p> 
         <p><b>Lake Size: </b>1270 acres</p>
@@ -146,8 +147,15 @@ const markers = [
     `)),
 ];
 
-// This plots each of the points to the map
-markers.forEach(marker => marker.addTo(map));
+for (let i = 0; i < markers.length; i++) {
+    const el = document.createElement('div');
+    el.className = 'marker' + i;
+    markers[i] = new mapboxgl.Marker(el)
+        .setLngLat(markers[i].getLngLat())
+        .setPopup(markers[i].getPopup())
+        .addTo(map);
+}
+
 
 // Coordinates for the box around the lake points
 const lineCoordinates = [
