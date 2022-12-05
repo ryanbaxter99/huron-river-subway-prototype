8b866a15e85954908945b620ebd5f281a28e53e6
ryanbaxter99 <rybaxter@umich.edu>
2022-12-04 22:04:14 -0500

    Added River Lines

--- js/index.js
+++ js/index.js
@@ -166,6 +166,36 @@ const lineCoordinates = [
     [-84, 42.3],        // back to start
 ];
 
+const riverLines = [
+    // Starting point at Big Lake 
+    [-83.51909881189316, 42.721293972977314], 
+     // Big Lake to Pontiac Lake
+    [-83.45103241607949, 42.664682472492984], 
+    // Pontiac Lake to Oxbow Lake
+    [-83.47983124810104, 42.645980588668685],
+    // Oxbow Lake to Ceder Island Lake
+    [-83.48099571145308, 42.63039758294164],
+    // Cedar Island Lake to North Commerce Lake
+    [-83.48986218111924, 42.58293304302951],
+    // North Commerce Lake to South Commerce Lake
+    [-83.49968514670081, 42.57435407009425],
+    // South Commerce Lake to Proud Lake
+    [-83.52097367057802, 42.56785846280867], 
+    // Proud Lake to Hubbell Lake
+    [-83.61555794821616, 42.58579168920394],
+    // Hubbell Lake to Kent Lake
+    [-83.64676344950117, 42.5289401382445],
+    // Kent Lake to Base Line Lake
+    [-83.8926796865478, 42.425370235294935], 
+    // Base Line Lake to Barton Pond
+    [-83.7584324082047, 42.31377611849595],
+    // Barton Pond to Ford Lake
+    [-83.60534047890552, 42.21892656979439],
+    // Ford Lake to Belleville Lake
+    [-83.49774610909319, 42.213428101123355],
+]
+
+
 // Runs on map load - adds the lines to the map
 map.on('load', () => {
     map.addSource('line', {
@@ -174,7 +204,7 @@ map.on('load', () => {
             type: 'Feature',
             geometry: {
                 type: 'LineString',
-                coordinates: lineCoordinates
+                coordinates: riverLines
             }
         }
     });
